87a69f155a5bd481bda3271fe9532ae98b6833c1
mohammed yusuf <mwaigalo5@gmail.com>
2018-05-03 22:08:44 +0300

    f

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,8 @@
 Function nav(){
     var name=("Enter your name");
     alert("Hi " +name);
+    var Age=prompt("How old are you?");
+    if(Age<18){
+        alert("This site is for over 18 please");
+    }
 }
