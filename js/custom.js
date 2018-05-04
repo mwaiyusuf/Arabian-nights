2189815cd6f0ad8151bbfe954a7d57b2911994a0
mohammed yusuf <mwaigalo5@gmail.com>
2018-05-04 20:55:04 +0300

    d

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@ Function nav(){
     alert("Hi " +name);
     var Age=prompt("How old are you?");
     if(Age<18){
-        alert("This site is for over 18 please");
+        alert("This site is for over 18yrs please");
     }
     else{
         alert("Welcome to this amazing site");
