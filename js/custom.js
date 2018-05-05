0f1ed2267d27481ac5733b3379e8d8bc7461f0d6
mohammed yusuf <mwaigalo5@gmail.com>
2018-05-05 10:12:57 +0300

    g

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,15 @@
-Function nav(){
-    var name=("Enter your name");
-    alert("Hi " +name);
-    var Age=prompt("How old are you?");
-    if(Age<18){
-        alert("This site is for over 18yrs please");
-    }
-    else{
-        alert("Welcome to this amazing site");
-    }
+//Function nav(){
+//    var name=("Enter your name");
+//    alert("Hi " +name);
+//    var Age=prompt("How old are you?");
+//    if(Age<18){
+//        alert("This site is for over 18yrs please");
+//    }
+//    else{
+//        alert("Welcome to this amazing site");
+//    }
+//}
+//nav();
+function bae(){
+    var int=prompt("Enter a number");
 }
-nav();
